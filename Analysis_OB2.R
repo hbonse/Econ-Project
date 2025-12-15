b93bae1bb31ab2d2ec88742cb5b7a3d199308deb
hbonse <hbonsey@hotmail.com>
2025-12-15 23:12:56 +0000

    More Updates

--- Analysis_OB2.R
+++ Analysis_OB2.R
@@ -1,9 +1,10 @@
 Combined_Data <- Combined_Data %>%
   mutate(
-    total_pc_2024 = Total_Capacity_2024 / Population_number * 1000
+    total_pc_2024 = Total_Capacity_2024 / Population_number * 1000,
+    total_pc_2014 = Total_Capacity_2014 / Population_number * 1000
   )
 
-
+library(ggplot2)
 OB2_data <- Shapefile %>%
   left_join(Combined_Data, by = c("LAD25CD" = "LAD code"))
 
@@ -36,3 +37,23 @@ ggplot(Combined_Data, aes(x = total_pc_2014, y = total_pc_2024)) +
     y = "Per-Capita Capacity (2024)",
     title = "Persistence of Renewable Energy Hotspots"
   )
+
+
+Combined_Data <- Combined_Data %>%
+  mutate(
+    pv_pc_2024 = Photovoltaics_2024 / Population_number * 1000,
+    onshore_pc_2024 = Onshore_Wind_2024 / Population_number * 1000,
+    offshore_pc_2024 = Offshore_Wind_2024 / Population_number * 1000
+  )
+
+
+#per capacity wind map
+ggplot(OB2_data %>% left_join(Combined_Data, by = "Name")) +
+  geom_sf(aes(fill = onshore_pc_2024), colour = NA) +
+  scale_fill_viridis_c() +
+  labs(
+    fill = "MW per 1,000 people",
+    title = "Per-Capita Onshore Wind Capacity by Local Authority (2024)"
+  ) +
+  theme_minimal()
+
--- Master_file.R
+++ Master_file.R
@@ -13,6 +13,8 @@ source("Clean_V2.R") #Cleans the data to make it is ready for merger
 
 source("Merge.R") #Merges all the clean data into one table
 
+#Time for analysis and visualisation!
+
 source("Analysis_OB1.R") #Analyses the data and creates visualisations)
 source("Analysis_OB2.R") #Analyses the data and creates visualisations)
 source("Analysis_OB3.R") #Analyses the data and creates visualisations)
@@ -25,6 +27,6 @@ head(Combined_Data) #to check this
 #You can now save this data as a CSV for future use
 write.csv(Combined_Data, "data/Combined_Data.csv", row.names = FALSE)
 
-#Time for analysis and visualisation!
+
 
 source("Analysis.R") #Time to analyse the data
